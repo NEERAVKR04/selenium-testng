db3bb270cf777cda42946a25c8af769385525b4e
NEERAVKR04 <30573865+NEERAVKR04@users.noreply.github.com>
2020-08-15 22:10:03 +0530

    Blog re-direction code

--- src/SampleTest.java
+++ src/SampleTest.java
@@ -3,6 +3,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import java.util.concurrent.TimeUnit;
+import java.lang.*;
+
+
 
 
 public class SampleTest {
@@ -16,7 +20,7 @@ public class SampleTest {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\Administrator\\Downloads\\chromedriver_win32\\chromedriver.exe");
         System.out.println("launching chrome browser");
         driver = new ChromeDriver();
-        driver.get("https://thoughtcoders.com");
+        driver.get(URL);
         String expWinTitle ="ThoughtCoders | Web Development, Automation, QA Training";
         String actWinTitle = driver.getTitle();
 
@@ -26,6 +30,20 @@ public class SampleTest {
 
     }
     @Test(priority = 2)
+    public void goToBlogs(){
+        driver.findElement(By.xpath("//a[@title='Blogs']")).click();
+//        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+//            driver.findElement(By.xpath("//a[contains(text(),'Top 25 Appium Interview Questions with Answers')]")).click();
+//        driver.switchTo().window("Blog | ThoughtCoders");
+        //*[@id="menu-item-662"]/a
+        String winUrl = driver.getCurrentUrl();
+        System.out.println(winUrl);
+
+        System.out.println(driver.getTitle());
+    }
+
+    @Test(priority = 3)
     public void closeConnections() {
         driver.quit();
 
