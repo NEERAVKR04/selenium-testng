267c6968c558cd9fd897e30ee3570e7e275b6b41
NEERAVKR04 <30573865+NEERAVKR04@users.noreply.github.com>
2020-08-16 19:22:16 +0530

    Resolved Blog page re-direction logic & added re-direction to single blog.

--- src/SampleTest.java
+++ src/SampleTest.java
@@ -1,12 +1,15 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import java.util.concurrent.TimeUnit;
 import java.lang.*;
-
-
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class SampleTest {
@@ -14,13 +17,17 @@ public class SampleTest {
     public String URL = "https://thoughtcoders.com";
     public WebDriver driver;
 
+
     @Test(priority = 1)
-    public void verifyHomepageTitle() {
+    public void verifyHomepageTitle() throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver","C:\\Users\\Administrator\\Downloads\\chromedriver_win32\\chromedriver.exe");
         System.out.println("launching chrome browser");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get(URL);
+        Thread.sleep(5000);
+
         String expWinTitle ="ThoughtCoders | Web Development, Automation, QA Training";
         String actWinTitle = driver.getTitle();
 
@@ -30,17 +37,25 @@ public class SampleTest {
 
     }
     @Test(priority = 2)
-    public void goToBlogs(){
-        driver.findElement(By.xpath("//a[@title='Blogs']")).click();
-//        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    public void goToBlogs() throws InterruptedException {
+        WebElement e;
+        Actions action = new Actions(driver);
+        e = driver.findElement(By.xpath("//a[@title='Blogs']"));
+        action.moveToElement(e).perform();
 
-//            driver.findElement(By.xpath("//a[contains(text(),'Top 25 Appium Interview Questions with Answers')]")).click();
-//        driver.switchTo().window("Blog | ThoughtCoders");
-        //*[@id="menu-item-662"]/a
+        WebDriverWait wait = new WebDriverWait(driver,20);
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@title='Blog']")));
+        e = driver.findElement(By.xpath("//a[@title='Blog']"));
+        e.click();
+        driver.manage().timeouts().pageLoadTimeout(30,TimeUnit.SECONDS);
         String winUrl = driver.getCurrentUrl();
         System.out.println(winUrl);
 
         System.out.println(driver.getTitle());
+
+       driver.findElement(By.xpath("//a[contains(text(),'Top 25 Appium Interview Questions with Answers')]")).click();
+       Thread.sleep(5000);
+
     }
 
     @Test(priority = 3)
